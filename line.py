b0284a712dce89152e94371a6adc2465ab7fbd1f
mifen <191615342@qq.com>
2023-12-07 11:15:19 +0800

    概率霍夫变换 HoughLinesP

--- line.py
+++ line.py
@@ -4,17 +4,19 @@ import numpy as np
 
 def line_detect(image_path, threshold=200):
     """检测图像中的线段
-    :param threshold: 线段最小长度，小于此值的不检测
+    :param threshold: 线段点数阈值，小于此值的不检测
+
+    https://blog.csdn.net/ftimes/article/details/106816736
+    简单场景下 HoughLines 够用，实际场景可能存在严重的误检
+    HoughLinesP 在复杂场景比 HoughLines 识别的更少直线
     """
     image = cv2.imread(image_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(
-        gray, 50, 150, apertureSize=3
-    )  # 边缘检测,50和150是两个用于确定边缘的强度阈值，apertureSize表示梯度计算的卷积核大小
+    # 边缘检测,50和150是两个用于确定边缘的强度阈值，apertureSize表示梯度计算的卷积核大小
+    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
     # cv2.imshow('edges', edges)
-    lines = cv2.HoughLines(
-        edges, 1, np.pi / 180, threshold
-    )  # 直线段检测,1表示每个像素点都有可能成为线段的端点。np.pi/180表示将弧度转换为度数，因为cv2.HoughLines函数接受的是弧度作为参数
+    # 直线段检测（标准霍夫变换）,edges是二值图像；1表示精度，即每个像素点都有可能成为线段的端点；np.pi/180 是角度搜索精度，表示搜索所有可能的角度
+    lines = cv2.HoughLines(edges, 1, np.pi / 180, threshold)
     for line in lines:
         for rho, theta in line:
             a = np.cos(theta)
@@ -28,6 +30,20 @@ def line_detect(image_path, threshold=200):
             cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
     cv2.imshow("detected lines", image)
     cv2.waitKey(0)  # Wait for a key press and close the window. Esc
+    cv2.destroyAllWindows()
+
+
+def line_detect_p(image_path, threshold=200, minLineLength=300, maxLineGap=100):
+    """使用概率霍夫变换（HoughLinesP）检测图像中的线段"""
+    image = cv2.imread(image_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold, minLineLength, maxLineGap)
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    cv2.imshow("detected lines", image)
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
@@ -108,6 +124,7 @@ def line_detect_direction(image_path, horizontal=True, vertical=True, angel_pi=8
 if __name__ == "__main__":
     image_path = "/tmp/test.jpg"
     line_detect(image_path)
+    line_detect_p(image_path)
     line_detect_padding(image_path, 50)
     line_detect_direction(image_path, True, False)
     line_detect_direction(image_path, False, True)
